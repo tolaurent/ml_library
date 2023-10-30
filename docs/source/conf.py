9877129872eabbff5336df65a9523b223d3ad581
Thomas Laurent <12704759+tolaurent@users.noreply.github.com>
2023-10-30 21:29:36 +0900

    Changing author information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Deep learning documentation'
+copyright = '2023 Thomas Laurent'
+author = 'Thomas Laurent'
 
 release = '0.1'
 version = '0.1.0'
